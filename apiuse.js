50d36ec71caa58cc26ac63ec99c415c037d69ac1
Isha2024 <109620959+Isha2024@users.noreply.github.com>
2023-07-15 11:08:52 -0400

    Update apiuse.js

--- apiuse.js
+++ apiuse.js
@@ -1,1 +1,20 @@
+var url = "https://jooble.org/api/";
+var key = "<YOUR_API_KEY>";
+var params = "{ keywords: 'it', location: 'Bern'}"
 
+//create xmlHttpRequest object
+var http = new XMLHttpRequest();
+//open connection. true - asynchronous, false - synchronous
+http.open("POST", url + key, true);
+
+//Send the proper header information
+http.setRequestHeader("Content-type", "application/json");
+	
+//Callback when the state changes
+http.onreadystatechange = function() {
+	if(http.readyState == 4 && http.status == 200) {
+		alert(http.responseText);
+	}
+}
+//Send request to the server
+http.send(params);
